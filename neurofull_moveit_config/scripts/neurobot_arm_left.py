8c25fc15a7fd77a349220a9cfc4312cc0bbf0319
Zhipeng Dong <zhipengdongneu@gmail.com>
2017-11-27 17:26:44 +0800

    add warn

--- neurofull_moveit_config/scripts/neurobot_arm_left.py
+++ neurofull_moveit_config/scripts/neurobot_arm_left.py
@@ -38,7 +38,7 @@ link_to_foot_ = 0.465
 # Initialize the move_group API
 moveit_commander.roscpp_initialize(sys.argv)
 
-rospy.sleep(6)  # Wait moveit to start up
+rospy.sleep(6)  # Wait moveit to start up, necessary!
 
 # Initialize the move group for the left arm
 left_arm = moveit_commander.MoveGroupCommander('left_arm')
