5a9db6a93ddb4f9a9fa27e45631ca27364594369
Zhipeng Dong <zhipengdongneu@gmail.com>
2017-11-30 12:06:15 +0800

    add time control, receive all traj and then publish

--- neuro_bridge/src/left_arm.cpp
+++ neuro_bridge/src/left_arm.cpp
@@ -30,12 +30,38 @@ typedef control_msgs::FollowJointTrajectoryActionGoal::ConstPtr TrajPtr;
 
 vector<TrajPtr> trajectories;
 
+// When the traj callback is receiving traj,
+// the transfer will not be executed
+bool on_receiving_ = false;
+
+// Record the last traj received time
+double lastTrajTime_ = 0.0;
+
 // Reduce the plan speed by this coeff
 double deceleration_ = 1.0;
+
+double delayTime_ = 1.0;
+
 void Callback(const TrajPtr& msg)
 {
-  ROS_INFO("Left arm: Trajectory plan received.");
   trajectories.push_back(msg);
+  on_receiving_ = true;
+}
+
+bool receiveTimeOut()
+{
+  if (on_receiving_) {
+    lastTrajTime_ = ros::Time::now().toSec();
+    return false; // Means have not been timeout
+  }
+  else {
+    double currTime = ros::Time::now().toSec();
+    if (currTime - lastTrajTime_ > delayTime_) {
+      return true;
+    }
+    else
+      return false;
+  }
 }
 
 int main(int argc,char **argv)
@@ -45,108 +71,120 @@ int main(int argc,char **argv)
   ros::NodeHandle pnh("~");
   
   pnh.getParam("deceleration", deceleration_);
+  pnh.getParam("delay", delayTime_);
   
   // The goal can be quick published, so the queue size should be reasonably bigger
-  ros::Subscriber sub = n.subscribe("/left_arm_controller/follow_joint_trajectory/goal", 200, Callback);
-
+  ros::Subscriber sub = n.subscribe("/left_arm_controller/follow_joint_trajectory/goal", 100, Callback);
+  
   // Use Twist message to communication with STM32
   geometry_msgs::Twist angle_state;
   geometry_msgs::Twist vel_state;
-
+  
   // Expand the queue for publishing is very important cause the traj can be transferred
   // to position and velocity command pretty quick, small queue will lead to lose intermediate data
-  ros::Publisher pub_angle_state = n.advertise<geometry_msgs::Twist>("my_trajectory_pos_left", 200);
-  ros::Publisher pub_vel_state = n.advertise<geometry_msgs::Twist>("my_trajectory_vel_left", 200);
-
+  ros::Publisher pub_angle_state = n.advertise<geometry_msgs::Twist>("my_trajectory_pos_left", 100);
+  ros::Publisher pub_vel_state = n.advertise<geometry_msgs::Twist>("my_trajectory_vel_left", 100);
+  
   // Result feedback
   ros::Publisher pub_result = n.advertise<std_msgs::Int8>("/feed/arm/left/move/result", 1);
-
+  
   while (n.ok()) {
-    if(!trajectories.empty()) {
-      TrajPtr act_msg;
-      ROS_INFO("Left arm: Get new trajectory from planning.");
+    if(!trajectories.empty() && !on_receiving_) {
+      ROS_INFO("Left arm: Get new trajectories from planning.");
       
-      // The act_msg contains all trajectories from planning
-      act_msg = trajectories.front();
-      // Clear the temp for the next planning
-      trajectories.erase(trajectories.begin());
-
-      uint pos_count = 0; // Count state number
-      int traj_size = act_msg->goal.trajectory.points.size();
-      
-      /**
-        The velocity and angle got from traj is instantaneous in each traj point,
-        including the start point and end point (velocity all equal to 0). However, the
-        driver of motor needs target position and speed in range between traj points,
-        besides, ##!!!! THE DRIVER ONLY RECOGNIZE POSITIVE SPEED , BUT THE SPEED 
-        FROM PLANNING CAN BE POSITIVE OR NEGATIVE, SO DO CONVENTION HERE !!!!##
-        **/
-
-      do {
-        // Velocity for current point and the next point
-        vector<double> vel_curr;
-        vector<double> vel_next;
+      for (size_t t = 0; t < trajectories.size(); ++t) {
+        TrajPtr act_msg;
         
-        // Time used for forwarding from current point to next point
-        ros::Duration secs_to_next;
+        // The act_msg contains 1 trajectory from planning
+        act_msg = trajectories[t];
         
-        // Angle for current point and the next point
-        vector<double> angle_curr;
-        vector<double> angle_next;
+        uint pos_count = 0; // Count state number
+        int traj_size = act_msg->goal.trajectory.points.size();
         
-        for(int i = 0; i < act_msg->goal.trajectory.joint_names.size(); i++) {
-          // For each joint, at state id = pos_count, get its state and fill it into temp
-          vel_curr.push_back(act_msg->goal.trajectory.points[pos_count].velocities[i]);
-          vel_next.push_back(act_msg->goal.trajectory.points[pos_count + 1].velocities[i]);
+        /**
+          The velocity and angle got from traj is instantaneous in each traj point,
+          including the start point and end point (velocity all equal to 0). However, the
+          driver of motor needs target position and speed in range between traj points,
+          besides, ##!!!! THE DRIVER ONLY RECOGNIZE POSITIVE SPEED , BUT THE SPEED 
+          FROM PLANNING CAN BE POSITIVE OR NEGATIVE, SO DO CONVENTION HERE !!!!##
+          **/
+        
+        do {
+          // Velocity for current point and the next point
+          vector<double> vel_curr;
+          vector<double> vel_next;
           
-          angle_curr.push_back(act_msg->goal.trajectory.points[pos_count].positions[i]);
-          angle_next.push_back(act_msg->goal.trajectory.points[pos_count + 1].positions[i]);
+          // Time used for forwarding from current point to next point
+          ros::Duration secs_to_next;
+          
+          // Angle for current point and the next point
+          vector<double> angle_curr;
+          vector<double> angle_next;
+          
+          for(int i = 0; i < act_msg->goal.trajectory.joint_names.size(); i++) {
+            // For each joint, at state id = pos_count, get its state and fill it into temp
+            vel_curr.push_back(act_msg->goal.trajectory.points[pos_count].velocities[i]);
+            vel_next.push_back(act_msg->goal.trajectory.points[pos_count + 1].velocities[i]);
+            
+            angle_curr.push_back(act_msg->goal.trajectory.points[pos_count].positions[i]);
+            angle_next.push_back(act_msg->goal.trajectory.points[pos_count + 1].positions[i]);
+          }
+          secs_to_next = act_msg->goal.trajectory.points[pos_count + 1].time_from_start
+              - act_msg->goal.trajectory.points[pos_count].time_from_start;
+          // Convert to Twist message, first publish velocity, then angle
+          // The velocity is the instantaneous velocity at each position
+          
+          // Method 1: use average speed calculated by: d/t
+          //vel_state.linear.x = (angle_next[0] - angle_curr[0])/secs_to_next.toSec() * deceleration_;
+          //vel_state.linear.y = (angle_next[1] - angle_curr[1])/secs_to_next.toSec() * deceleration_;
+          //vel_state.linear.z = (angle_next[2] - angle_curr[2])/secs_to_next.toSec() * deceleration_;
+          //vel_state.angular.x = (angle_next[3] - angle_curr[3])/secs_to_next.toSec() * deceleration_;
+          //vel_state.angular.y = (angle_next[4] - angle_curr[4])/secs_to_next.toSec() * deceleration_;
+          //vel_state.angular.z = (angle_next[5] - angle_curr[5])/secs_to_next.toSec() * deceleration_;
+          
+          // Method 2: use max speed among current and next
+          vel_state.linear.x = fabs(vel_curr[0])>fabs(vel_next[0])?fabs(vel_curr[0]):fabs(vel_next[0]);
+          vel_state.linear.y = fabs(vel_curr[1])>fabs(vel_next[1])?fabs(vel_curr[1]):fabs(vel_next[1]);
+          vel_state.linear.z = fabs(vel_curr[2])>fabs(vel_next[2])?fabs(vel_curr[2]):fabs(vel_next[2]);
+          vel_state.angular.x = fabs(vel_curr[3])>fabs(vel_next[3])?fabs(vel_curr[3]):fabs(vel_next[3]);
+          vel_state.angular.y = fabs(vel_curr[4])>fabs(vel_next[4])?fabs(vel_curr[4]):fabs(vel_next[4]);
+          vel_state.angular.z = fabs(vel_curr[5])>fabs(vel_next[5])?fabs(vel_curr[5]):fabs(vel_next[5]);
+          
+          angle_state.linear.x = angle_next[0];
+          angle_state.linear.y = angle_next[1];
+          angle_state.linear.z = angle_next[2];
+          angle_state.angular.x = angle_next[3];
+          angle_state.angular.y = angle_next[4];
+          angle_state.angular.z = angle_next[5];
+          
+          pub_vel_state.publish(vel_state);
+          pub_angle_state.publish(angle_state);
+          
+          // No nedd to add time delay
+          // Prepare for the next state
+          pos_count++;
         }
-        secs_to_next = act_msg->goal.trajectory.points[pos_count + 1].time_from_start
-                       - act_msg->goal.trajectory.points[pos_count].time_from_start;
-        // Convert to Twist message, first publish velocity, then angle
-        // The velocity is the instantaneous velocity at each position
-        
-        // Method 1: use average speed calculated by: d/t
-        //vel_state.linear.x = (angle_next[0] - angle_curr[0])/secs_to_next.toSec() * deceleration_;
-        //vel_state.linear.y = (angle_next[1] - angle_curr[1])/secs_to_next.toSec() * deceleration_;
-        //vel_state.linear.z = (angle_next[2] - angle_curr[2])/secs_to_next.toSec() * deceleration_;
-        //vel_state.angular.x = (angle_next[3] - angle_curr[3])/secs_to_next.toSec() * deceleration_;
-        //vel_state.angular.y = (angle_next[4] - angle_curr[4])/secs_to_next.toSec() * deceleration_;
-        //vel_state.angular.z = (angle_next[5] - angle_curr[5])/secs_to_next.toSec() * deceleration_;
-        
-        // Method 2: use max speed among current and next
-        vel_state.linear.x = fabs(vel_curr[0])>fabs(vel_next[0])?fabs(vel_curr[0]):fabs(vel_next[0]);
-        vel_state.linear.y = fabs(vel_curr[1])>fabs(vel_next[1])?fabs(vel_curr[1]):fabs(vel_next[1]);
-        vel_state.linear.z = fabs(vel_curr[2])>fabs(vel_next[2])?fabs(vel_curr[2]):fabs(vel_next[2]);
-        vel_state.angular.x = fabs(vel_curr[3])>fabs(vel_next[3])?fabs(vel_curr[3]):fabs(vel_next[3]);
-        vel_state.angular.y = fabs(vel_curr[4])>fabs(vel_next[4])?fabs(vel_curr[4]):fabs(vel_next[4]);
-        vel_state.angular.z = fabs(vel_curr[5])>fabs(vel_next[5])?fabs(vel_curr[5]):fabs(vel_next[5]);
-        
-        angle_state.linear.x = angle_next[0];
-        angle_state.linear.y = angle_next[1];
-        angle_state.linear.z = angle_next[2];
-        angle_state.angular.x = angle_next[3];
-        angle_state.angular.y = angle_next[4];
-        angle_state.angular.z = angle_next[5];
-        
-        pub_vel_state.publish(vel_state);
-        pub_angle_state.publish(angle_state);
-        
-        // Add time delay to make sure the plan is executed
-        
-        // Prepare for the next state
-        pos_count++;
+        while(pos_count < traj_size - 1); // Omit the last point's velocity cause it is 0
       }
-      while(pos_count < traj_size - 1); // Omit the last point's vel
-
+      // All traj have been published, so we need clear the temp
+      trajectories.clear();
+      
+      // Publish feedback
       std_msgs::Int8 flag;
       flag.data = 1;
       pub_result.publish(flag);
       ROS_INFO("Left arm: Publish trajectory plan finished.");
     }
-
+    
+    // Reset the receive status before each loop
+    on_receiving_ = false;
     ros::spinOnce();
+    
+    // Judge whether the traj have been completely received
+    if (receiveTimeOut)
+      on_receiving_ = false;
+    else
+      on_receiving_ = true;
   }
   return 0;
 }
